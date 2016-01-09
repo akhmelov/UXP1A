c576d822b65b67df048f938dc17c7c37b36df187
Wiktor Kuśmirek <wiktor93@onet.com.pl>
2016-01-09 18:11:27 +0100

    znak zachety poprzedzony sciezka do aktualnego katalog + dzialajacy 'cd'

--- main.cpp
+++ main.cpp
@@ -5,18 +5,18 @@
 #include <string.h>
 #include <ctype.h>
 #include <unistd.h>
-
+ 
 #include <sys/types.h>
 #include <sys/wait.h>
-
+ 
 /* The array below will hold the arguments: args[0] is the command. */
 static char* args[512];
 pid_t pid;
 int command_pipe[2];
-
+ 
 #define READ  0
 #define WRITE 1
-
+ 
 /*
  * Handle commands separatly
  * input: return value from previous command (useful for pipe file descriptor)
@@ -34,16 +34,16 @@ int command_pipe[2];
 static int command(int input, int first, int last)
 {
 	int pipettes[2];
-
+ 
 	/* Invoke pipe */
-	pipe( pipettes );
+	pipe( pipettes );	
 	pid = fork();
-
+ 
 	/*
 	 SCHEME:
 	 	STDIN --> O --> O --> O --> STDOUT
 	*/
-
+ 
 	if (pid == 0) {
 		if (first == 1 && last == 0 && input == 0) {
 			// First command
@@ -56,61 +56,66 @@ static int command(int input, int first, int last)
 			// Last command
 			dup2( input, STDIN_FILENO );
 		}
-
+ 
 		if (execvp( args[0], args) == -1)
 			_exit(EXIT_FAILURE); // If child fails
 	}
-
-	if (input != 0)
+ 
+	if (input != 0) 
 		close(input);
-
+ 
 	// Nothing more needs to be written
 	close(pipettes[WRITE]);
-
+ 
 	// If it's the last command, nothing more needs to be read
 	if (last == 1)
 		close(pipettes[READ]);
-
+ 
 	return pipettes[READ];
 }
-
+ 
 /* Final cleanup, 'wait' for processes to terminate.
  *  n : Number of times 'command' was invoked.
  */
 static void cleanup(int n)
 {
 	int i;
-	for (i = 0; i < n; ++i)
-		wait(NULL);
+	for (i = 0; i < n; ++i) 
+		wait(NULL); 
 }
-
+ 
 static int run(char* cmd, int input, int first, int last);
 static char line[1024];
 static int n = 0; /* number of calls to 'command' */
-
+ 
 int main()
 {
 	printf("SIMPLE SHELL: Type 'exit' or send EOF to exit.\n");
 	while (1) {
 		/* Print the command prompt */
-		printf("$> ");
+		char cwd[1024];
+		if (getcwd(cwd, sizeof(cwd)) != NULL) {
+			printf("%s$> ", cwd);
+		} else {
+			printf("$> ");
+		}
 		fflush(NULL);
-
+ 
 		/* Read a command line */
-		if (!fgets(line, 1024, stdin))
+		if (!fgets(line, 1024, stdin)) 
 			return 0;
-
+ 
 		int input = 0;
 		int first = 1;
-
+ 
 		char* cmd = line;
 		char* next = strchr(cmd, '|'); /* Find first '|' */
-
+ 
 		while (next != NULL) {
 			/* 'next' points to '|' */
-			*next = '\0'; //set end of array
+			*next = '\0';
 			input = run(cmd, input, first, 0);
-
+ 
 			cmd = next + 1;
 			next = strchr(cmd, '|'); /* Find next '|' */
 			first = 0;
@@ -121,47 +126,52 @@ int main()
 	}
 	return 0;
 }
-
+ 
 static void split(char* cmd);
-
+ 
 static int run(char* cmd, int input, int first, int last)
 {
 	split(cmd);
 	if (args[0] != NULL) {
-		if (strcmp(args[0], "exit") == 0)
+		if (strcmp(args[0], "exit") == 0) 
 			exit(0);
+		if (strcmp(args[0], "cd") == 0) {	//for 'cd' command support, which is not executable file
+			if(chdir(args[1]) == -1) {
+				printf("Error occured while changing directory to %s\n", args[1]);
+			}
+		}
 		n += 1;
 		return command(input, first, last);
 	}
 	return 0;
 }
-
+ 
 static char* skipwhite(char* s)
 {
 	while (isspace(*s)) ++s;
 	return s;
 }
-
+ 
 static void split(char* cmd)
 {
-	cmd = skipwhite(cmd); //removes white space from beginning, clear command
+	cmd = skipwhite(cmd);
 	char* next = strchr(cmd, ' ');
 	int i = 0;
-
+ 
 	while(next != NULL) {
-		next[0] = '\0'; // *next
+		next[0] = '\0';
 		args[i] = cmd;
 		++i;
 		cmd = skipwhite(next + 1);
 		next = strchr(cmd, ' ');
 	}
-
+ 
 	if (cmd[0] != '\0') {
 		args[i] = cmd;
 		next = strchr(cmd, '\n');
 		next[0] = '\0';
-		++i;
+		++i; 
 	}
-
+ 
 	args[i] = NULL;
 }
